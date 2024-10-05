c4b039a067afe61693a5c75bf822245e5246959e
Borys Lopez <68643829+boryslopez02@users.noreply.github.com>
2024-10-05 00:31:57 -0400

    base

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+    return (
+        <>
+            <h1>Hello im react</h1>
+            <img src="https://picsum.photos/200/300" alt="" />
+        </>
+    )
+}
+
+export default App;
