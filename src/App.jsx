de8984cdb1b24a3e054b712941759e177f22dac9
Borys Lopez <68643829+boryslopez02@users.noreply.github.com>
2024-10-23 12:22:23 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,45 @@
+import img from "./assets/images/1026-200x300.jpg";
+import PropTypes from 'prop-types';
+
+
+const WelcomeText = ({user}) => (user ? <h3>Online</h3> : <h3>Offline</h3>);
+
+const MyButton = ({text}) => {
+    return <button>{text}</button>
+};
+
+MyButton.propTypes = {
+    text: PropTypes.string.isRequired,
+}
+
+const ItemFruits = (props) => {
+    return (
+        <li>{props.fruit}</li>
+    )
+}
+
 const App = () => {
+    const title = "Titulo de prueba";
+    const fruits = ["🍉", "🍊", "🍋", "🍍", "🍒"];
+    let user = true;
+
+
     return (
         <>
-            <h1>Hello im react</h1>
-            <img src="https://picsum.photos/200/300" alt="" />
+            <h1>{title}</h1>
+            <img src={img} />
+
+            <WelcomeText user={user}/>
+
+            <ul>
+                {
+                    fruits.map((fruit, index) => (
+                        <ItemFruits key={index} fruit={fruit} />
+                    ))
+                }
+            </ul>
+
+            <MyButton text='Save'/>
         </>
     )
 }
